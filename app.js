0d0fcdc6b0d7f30205b8a06f753ade5195228e36
mingmoth <mingmoth@gmail.com>
2021-09-14 20:09:25 +0800

    project init

---
+++ app.js
@@ -0,0 +1,18 @@
+const { urlencoded } = require('express')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const PORT = 3000
+
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.send('Hi')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})

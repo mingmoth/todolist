fdaee12774a1a7f8a891439cacdc11251efe46a3
mingmoth <mingmoth@gmail.com>
2021-09-14 21:46:59 +0800

    add mongoose and seeder

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const { urlencoded } = require('express')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 
-app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+const mongoose = require('mongoose') 
+mongoose.connect('mongodb://localhost/todolist', { useNewUrlParser: true, useUnifiedTopology: true })
+// 把這個連線狀態透過 const db = mongoose.connection 儲存到 db 這個物件
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.send('Hi')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Todo = require('../todo')
+
+mongoose.connect('mongodb://localhost/todolist', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+  for (let i = 0; i< 10; i++) {
+    Todo.create({name: 'todo-' + i})
+  }
+  console.log('Seed Done')
+})
